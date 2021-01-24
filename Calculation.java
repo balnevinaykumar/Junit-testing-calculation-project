1b4483ec26cc0c37253798ea788e7d6d250a9709
Vinay Kumar reddy balne <52401245+balnevinaykumar@users.noreply.github.com>
2021-01-24 21:51:18 +0530

    Add files via upload

---
+++ Calculation.java
@@ -0,0 +1,22 @@
+package test;
+
+public class Calculation {
+	public int add(int a, int b)
+	{
+		return a+b;
+	}
+	public int subtract(int a, int b)
+	{
+		return a-b;
+	}
+	public int multiply(int a, int b)
+	{
+		return a*b;
+	}
+	public int division(int a, int b)
+	{
+		return a/b;
+	}
+	
+
+}
